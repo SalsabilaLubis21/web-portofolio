a9fb6e472b5e0f263912734588fbe2a2f791d191
SalsabilaLubis21 <salsabila21022005@gmail.com>
2026-01-14 22:41:58 +0700

    fix: resolve build errors for deployment

--- app/api/data/route.js
+++ app/api/data/route.js
@@ -1,11 +1,16 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-static";
+
 export async function GET(request) {
-  return NextResponse.json({
-    success: true,
-    message: 'hle!',
-    data: {
-      message: 'Message and email sent successfully!',
-    }
-  }, { status: 200 });
-};
+  return NextResponse.json(
+    {
+      success: true,
+      message: "hle!",
+      data: {
+        message: "Message and email sent successfully!",
+      },
+    },
+    { status: 200 }
+  );
+}
